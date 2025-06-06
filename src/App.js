703d04e8c48cf188fad70cd5e7caa953f0cdeaef
hariprasath <hariprasathjagadheesh03@gmail.com>
2025-06-06 19:21:14 +0530

    Fix: updated footer links and cleaned unused variables

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <div className="App">
        <Navbar/>
-       <Carsoul/>
+       {/* <Carsoul/> */}
        <Cart/>
        <Collection/>
        {/* <Register/> */}
-       <Footer/>
+       {/* <Footer/> */}
        
     </div>
   );
